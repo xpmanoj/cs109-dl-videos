2271574b7ca1f648b222afcebd0da476106fc5db
vaishaks <vaishak93@gmail.com>
2014-12-17 15:44:38 +0530

    Download videos one by one.

--- get.sh
+++ get.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "Input the video number that you want to download: "
+num=0
+read num
+echo "Resume? (y/n)"
+rseume="n"
+read resume
 idx=1
 while read line; do
-	#echo $line
-	#echo $idx
-	rtmpdump -r rtmp://flash.dce.harvard.edu/bounce -C B:0 -C Z: -C S:/$line -C S:BounceAPI3.0 -C N:0.000000 -C S:mp4 -y mp4:$line -o video$idx.mp4
-	idx=$((idx + 1)) 
+	if [ $num == $idx ]; then
+		if [ $resume == "y" ]; then
+			rtmpdump -r rtmp://flash.dce.harvard.edu/bounce -C B:0 -C Z: -C S:/$line -C S:BounceAPI3.0 -C N:0.000000 -C S:mp4 -y mp4:$line -o video$idx.mp4 -e
+		else
+			rtmpdump -r rtmp://flash.dce.harvard.edu/bounce -C B:0 -C Z: -C S:/$line -C S:BounceAPI3.0 -C N:0.000000 -C S:mp4 -y mp4:$line -o video$idx.mp4
+		fi
+	fi
+	idx=$((idx + 1))
 done < $1
